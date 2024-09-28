1e396beab332436529ce5b9f9e6b062739e9cff9
Ahmad Ibrahim <iahmd01@gmail.com>
2024-09-28 16:31:40 +0300

    done hello world during lecture 3

--- hello-world/hello-world.asm
+++ hello-world/hello-world.asm
@@ -1,7 +1,20 @@
 global main
+extern printf
 
 section .text
 
 main:
-    mov eax, 0
+    mov eax, 42
+    push eax
+    push format
+    call printf
+    add esp, 8
+
+    xor eax, eax
     ret
+
+; may also be .rdata (or .data for mutable data)
+section .rodata
+format:
+    db "Hello, world! The number is %d", 0Ah, 0
+; db - for bytes, dw - words, dd - double words
